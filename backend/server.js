2a3617f595a64827af190ec854bd9a635c6fc172
Sagar Dulal <sagardulal100@gmail.com>
2022-11-28 17:44:41 +0545

    Server setup and mongdb src connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const app = express();
+const dotenv =require("dotenv")
+const connectDB = require('./config/connectDB')
+dotenv.config();
+
+
+//Routes
+app.get("/", (req, res)=>{
+    res.send("Home Page");
+})
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+
+
+const startServer = async() =>{
+    try{
+        await connectDB();
+        app.listen(PORT,()=>{
+            console.log(`Server running on port ${PORT}`)
+        })
+    }
+    catch(error){
+        console.log(error.message)
+    }
+};
+startServer();
+
+
